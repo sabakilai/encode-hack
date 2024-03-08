ef16e1927a6b2a042540350d6aafcbe654c765c1
Dima Dem <iamdimadem@gmail.com>
2024-03-08 20:02:25 +0000

    add: camera, add: pages

--- frontend/webapp/src/app/createAccount/page.tsx
+++ frontend/webapp/src/app/createAccount/page.tsx
@@ -1,14 +1,11 @@
 'use client';
 
-import { useState } from "react";
-
+import React, { useEffect, useRef, useCallback, useState } from "react";
+import { set, get, update, del } from 'idb-keyval';
 import { Button } from "@/shared/ui";
-import { Camera } from "@/entities/camera";
+import Link from "next/link";
 
 const CreateAccountPage = () => {
-  const [isCameraOpen, setIsCameraOpen] = useState(false);
-  const [cardImage, setCardImage] = useState();
-
   return (
     <main className="flex min-h-svh flex-col bg-gradient-to-t from-indigo-300 via-pink-200 p-12">
       <h1 className="text-2xl font-bold">Create Account</h1>
@@ -18,3 +15,97 @@ const CreateAccountPage = () => {
 };
 
 export default CreateAccountPage;
+
+const Camera = () => {
+  const [selfie, setSelfie] = useState<boolean>(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const photoRef = useRef<HTMLCanvasElement>(null);
+
+  const getUserCamera = async () => {
+    navigator.mediaDevices.getUserMedia({ video: { width: 300, height: 300 }, audio: false })
+      .then((stream) => {
+        if (!videoRef.current) return
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play().catch((err) => console.error("Error playing video:", err));
+      })
+      .catch((err) => {
+        console.error("Error accessing the camera", err);
+      });
+  }
+
+  const handleTakePhoto = useCallback(() => {
+    if (!videoRef.current || !photoRef.current) return;
+    const width = 300;
+    const height = width / (1 / 1);
+    let photo = photoRef.current;
+    let video = videoRef.current;
+    let ctx = photo.getContext("2d");
+    photo.width = width;
+    photo.height = height;
+    ctx?.drawImage(video, 0, 0, photo.width, photo.height);
+    let data = photo.toDataURL("image/png");
+    set("selfie", data);
+    setSelfie(true)
+    console.log("selfie created");
+    if (videoRef.current && photoRef.current) {
+      videoRef.current.style.display = 'none';
+      photoRef.current.style.display = 'block';
+    }
+  }, [videoRef, photoRef]);
+
+  const handleClearPhoto = useCallback(() => {
+    if (!photoRef.current) return;
+    let photoCurrent = photoRef.current;
+    let ctx = photoCurrent?.getContext("2d");
+    ctx?.clearRect(0, 0, photoCurrent.width, photoCurrent.height);
+    del("selfie");
+    setSelfie(false);
+    console.log("selfie cleared");
+    if (videoRef.current && photoRef.current) {
+      videoRef.current.style.display = 'block';
+      photoRef.current.style.display = 'none';
+    }
+  }, [photoRef]);
+
+  useEffect(() => {
+    getUserCamera();
+  }, [videoRef]);
+
+  return (
+    <div className="flex flex-col relative">
+      <div className="py-4"></div>
+      <div className="flex justify-center">
+        {videoRef && <video
+          playsInline
+          autoPlay
+          className="border-white rounded-full border-8 h-[300px] w-[300px]"
+          ref={videoRef}>
+        </video>}
+      </div>
+      <div className="mx-auto">
+        {photoRef && <canvas
+          className="border-white hidden border-8 h-[300px] w-[300px]"
+          ref={photoRef}>
+        </canvas>}
+      </div>
+      <div className="py-4"></div>
+      <div className="flex w-full mb-8 justify-evenly">
+        {selfie == false ?
+          (<Button onClick={handleTakePhoto}>Take photo</Button>) :
+          (<Button variant={"destructive"} onClick={handleClearPhoto}>Clear photo</Button>)
+        }
+      </div>
+      <div className="py-12"></div>
+      <div className="flex w-full justify-center">
+        {selfie == true &&
+          <Button className="w-fit">
+            <Link href="/scanIngredients">
+              Next
+            </Link>
+          </Button>
+        }
+      </div>
+    </div>
+  )
+}
--- frontend/webapp/src/app/scanIngredients/page.tsx
+++ frontend/webapp/src/app/scanIngredients/page.tsx
@@ -1,24 +1,33 @@
-import React, { useEffect, useRef, useCallback } from "react";
+'use client';
+
+import React, { useEffect, useRef, useCallback, useState } from "react";
 import { set, get, update, del } from 'idb-keyval';
-import { createAppStore } from "@/shared/storage/storage";
 import { Button } from "@/shared/ui";
+import Link from "next/link";
 
-interface ICamera {
-}
+const CreateAccountPage = () => {
+    return (
+        <main className="flex min-h-svh flex-col bg-gradient-to-t from-indigo-300 via-pink-200 p-12">
+            <h1 className="text-2xl font-bold">Scan Ingredients</h1>
+            <Camera />
+        </main>
+    );
+};
 
-const Camera = ({ }: ICamera) => {
-    // const [photo, setPhoto] = useState<string>("");
+export default CreateAccountPage;
+
+const Camera = () => {
+    const [ingredients, setIngredients] = useState<boolean>(false);
     const videoRef = useRef<HTMLVideoElement>(null);
     const photoRef = useRef<HTMLCanvasElement>(null);
-    //! STREAMING CAMERA
+
     const getUserCamera = async () => {
-        navigator.mediaDevices.getUserMedia({ video: { width: 300, height: 300 }, audio: false })
+        navigator.mediaDevices.getUserMedia({ video: { width: 300, height: 300, facingMode: "environment" }, audio: false })
             .then((stream) => {
                 if (!videoRef.current) return
                 let video = videoRef.current;
                 video.srcObject = stream;
                 video.play().catch((err) => console.error("Error playing video:", err));
-
             })
             .catch((err) => {
                 console.error("Error accessing the camera", err);
@@ -27,7 +36,7 @@ const Camera = ({ }: ICamera) => {
 
     const handleTakePhoto = useCallback(() => {
         if (!videoRef.current || !photoRef.current) return;
-        const width = 280;
+        const width = 300;
         const height = width / (1 / 1);
         let photo = photoRef.current;
         let video = videoRef.current;
@@ -36,20 +45,27 @@ const Camera = ({ }: ICamera) => {
         photo.height = height;
         ctx?.drawImage(video, 0, 0, photo.width, photo.height);
         let data = photo.toDataURL("image/png");
-        // setPhoto(data);
-        set("selfie", data);
-        console.log("selfie created");
+        set("ingredients", data);
+        setIngredients(true)
+        console.log("ingredients created");
+        if (videoRef.current && photoRef.current) {
+            videoRef.current.style.display = 'none';
+            photoRef.current.style.display = 'block';
+        }
     }, [videoRef, photoRef]);
-    // }, [videoRef, photoRef, setPhoto]);
 
     const handleClearPhoto = useCallback(() => {
         if (!photoRef.current) return;
         let photoCurrent = photoRef.current;
         let ctx = photoCurrent?.getContext("2d");
         ctx?.clearRect(0, 0, photoCurrent.width, photoCurrent.height);
-        // setPhoto("");
-        del("selfie");
-        console.log("selfie cleared");
+        del("ingredients");
+        setIngredients(false);
+        console.log("ingredients cleared");
+        if (videoRef.current && photoRef.current) {
+            videoRef.current.style.display = 'block';
+            photoRef.current.style.display = 'none';
+        }
     }, [photoRef]);
 
     useEffect(() => {
@@ -63,24 +79,30 @@ const Camera = ({ }: ICamera) => {
                 {videoRef && <video
                     playsInline
                     autoPlay
-                    className="rounded-full border-white border-8 h-[300px] w-[300px]"
+                    className="border-white rounded-full border-8 h-[300px] w-[300px]"
                     ref={videoRef}>
                 </video>}
             </div>
-            <div className="py-4"></div>
             <div className="mx-auto">
                 {photoRef && <canvas
-                    className="rounded-3xl border-white border-8 h-[280px] w-[280px]"
+                    className="border-white hidden border-8 h-[300px] w-[300px]"
                     ref={photoRef}>
                 </canvas>}
             </div>
             <div className="py-12"></div>
             <div className="flex w-full mb-8 justify-evenly">
-                <Button onClick={handleTakePhoto}>Take photo</Button>
-                <Button onClick={handleClearPhoto}>Clear photo</Button>
+                {ingredients == false ?
+                    (<Button onClick={handleTakePhoto}>Take photo</Button>) :
+                    (<Button onClick={handleClearPhoto}>Clear photo</Button>)
+                }
+                {ingredients == true &&
+                    <Button className="w-fit">
+                        <Link href="/">
+                            Next
+                        </Link>
+                    </Button>
+                }
             </div>
         </div >
     )
 }
-
-export { Camera }
