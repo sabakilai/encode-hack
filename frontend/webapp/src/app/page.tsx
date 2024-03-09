d3df08085a12379491e6cf6e236929272b07d676
Dima Dem <iamdimadem@gmail.com>
2024-03-09 17:48:22 +0000

    add: selfie first feature

--- frontend/webapp/src/app/page.tsx
+++ frontend/webapp/src/app/page.tsx
@@ -1,17 +1,38 @@
 "use client";
 
+import { useEffect } from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { get } from 'idb-keyval';
 
-import { useState, useEffect } from "react";
-import { Emoji, Button, Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger, ScrollArea } from "@/shared/ui";
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  ScrollArea
+} from "@/shared/ui";
 import { IoEnter } from "react-icons/io5";
 
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Проверяем, загружен ли файл (например, в localStorage)
+    get('selfie').then((data) => {
+      if (data) {
+        router.push('/scanIngredients');
+      }
+    });
+  }, []);
+
   return (
     <main className="flex min-h-svh flex-col items-center justify-evenly p-6 relative overflow-hidden antialiased">
       <div className="space-y-16 h-full z-0">
-        {/* <div className="space-y-4"></div> */}
         <h1 className="text-right text-7xl font-bold">
           FC
           <br />
@@ -35,7 +56,6 @@ export default function Home() {
             <IoEnter size={60} />
           </Link>
         </Button>
-        {/* <div className="space-y-4"></div> */}
       </div>
     </main>
   );
@@ -53,7 +73,7 @@ const LearnMore = () => {
           <DialogTitle>Are you absolutely sure?</DialogTitle>
         </DialogHeader>
         <DialogDescription asChild>
-          <ScrollArea className="h-[80svh] w-[80svw]">
+          <ScrollArea className="h-[80svh] w-full">
             This action cannot be undone. This will permanently delete your account
             and remove your data from our servers.
             This action cannot be undone. This will permanently delete your account
