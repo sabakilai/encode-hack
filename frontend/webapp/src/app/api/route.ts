8aa4d82cc214649669369d7d846748c89df0ebdf
Dima Dem <iamdimadem@gmail.com>
2024-03-09 08:48:17 +0000

    fix: update route.ts

--- frontend/webapp/src/app/api/route.ts
+++ frontend/webapp/src/app/api/route.ts
@@ -1,7 +1,7 @@
 import { type Base64 } from "@/shared/types/basic";
 import { set } from "idb-keyval";
 
-export async function POST(selfie: Base64, ingredients: Base64) {
+export async function POST(request: Request, { selfie, ingredients }: { selfie: Base64, ingredients: Base64 }) {
     const res = await fetch('http://127.0.0.1:5000/userInput', {
         method: 'POST',
         headers: {
@@ -10,7 +10,6 @@ export async function POST(selfie: Base64, ingredients: Base64) {
         body: JSON.stringify({ user_photo: selfie, ingredients_photo: ingredients }),
     });
     const data = await res.text();
-    set("response", data);
-    return Response.json(data);
+    await set("response", data);
 }
 
