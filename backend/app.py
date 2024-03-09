98d3efae1e09d2f87b64fc3ecb33d88dfa5f09db
Jekaterina Usacova <kateusacova@gmail.com>
2024-03-09 16:18:16 +0000

    Remove counter logic

--- backend/app.py
+++ backend/app.py
@@ -14,23 +14,17 @@ model = FoodImageClassifier()
 def handle_user_input():
     data = request.get_json()
 
-    bad_counter = data['bad_counter']
-    good_counter = data['good_counter']
-    attempts_counter = data['attempts_counter']
-
-    base_64_to_image(data, 'user_photo')
+    # base_64_to_image(data, 'user_photo')
     base_64_to_image(data, 'ingredients_photo')
 
     food_category = model.predict('ingredients_photo.png')
-
+    transformed_photo = data['user_photo']
+    
     # Change to StabilityAI model
-    transformed_photo = model.predict(food_category, good_counter, bad_counter, attempts_counter)
+    # transformed_photo = model.predict(food_category, good_counter, bad_counter, attempts_counter)
     
     response = {
         'food_category': food_category,
-        'good_counter': good_counter,
-        'bad_counter': bad_counter,
-        'attempts_counter': attempts_counter,
         'transformed_photo': transformed_photo
     }
 
@@ -49,20 +43,3 @@ def base_64_to_image(data: dict, key: str):
     with open(key + '.png', 'wb') as f:
         f.write(img_data)
 
-
-def assign_counter_values(food_category, good_counter, bad_counter, attempts_counter):
-    counter_values = {
-        'Ultra-Processed Foods': (3, 0, 1),
-        'Processed Foods': (1, 0, 1),
-        'Processed Culinary Ingredients': (1, 0, 1),
-        'Unprocessed and Minimally Processed Foods': (0, 3, 1),
-        'Unable to identify the food category': (0, 0, 0)
-    }
-
-    if food_category in counter_values:
-        good_increment, bad_increment, attempts_increment = counter_values[food_category]
-        good_counter += good_increment
-        bad_counter += bad_increment
-        attempts_counter += attempts_increment
-    else:
-        return 'Invalid food category', 400
